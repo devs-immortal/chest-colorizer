6afb90416c59ce6ea967f9d0d4d60f3c7f62c35a
kalucky0 <kalucki23@gmail.com>
2023-04-27 00:32:53 +0200

    Remove chest color on destruction

---
+++ src/main/java/net/immortaldevs/colorizer/mixin/BlockMixin.java
@@ -0,0 +1,20 @@
+package net.immortaldevs.colorizer.mixin;
+
+import net.immortaldevs.colorizer.ColorizedChest;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.ChestBlock;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.WorldAccess;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(Block.class)
+public class BlockMixin {
+    @Inject(at = @At("HEAD"), method = "onBroken(Lnet/minecraft/world/WorldAccess;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;)V")
+    public void onBroken(WorldAccess world, BlockPos pos, BlockState state, CallbackInfo info) {
+        if ((Object) this instanceof ChestBlock) ColorizedChest.clearChestColor(pos);
+    }
+}
